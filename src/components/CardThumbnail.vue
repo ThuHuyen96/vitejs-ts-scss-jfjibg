4ebbe06116f652e71dbdeff6d2667112c64efd06
ThuHuyen96 <30973667+ThuHuyen96@users.noreply.github.com>
2024-09-09 14:26:58 +0700

    Initial commit

---
+++ src/components/CardThumbnail.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const msg = ref('Product name is quite long in some cases');
+</script>
+
+<template>
+  <div>
+    <img src="https://imgur.com/2MM9Jqo" />
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
